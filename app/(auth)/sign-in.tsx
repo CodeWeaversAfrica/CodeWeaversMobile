4a828dcf6d29cf551881ce830b5e38a5d444d268
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-08 21:17:35 +0300

    Project structure restructure: add app/(auth)/

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function SignIn()  {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold">Welcome  signin</Text>
+    </View>
+  );
+}
+
