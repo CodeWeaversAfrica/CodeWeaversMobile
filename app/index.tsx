e39cda3b55105c8175021d4124fdd4407b2ce826
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-08 21:17:35 +0300

    Project structure restructure: add app/index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+// import { useAuth } from "@clerk/clerk-expo";
+// import { Redirect } from "expo-router";
+
+// const Page = () => {
+//   const { isSignedIn } = useAuth();
+
+//   if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+//   return <Redirect href="/(auth)/welcome" />;
+// };
+
+// export default Page;
+
+
+// import { Redirect } from "expo-router";
+
+// const Page = () => {
+//   return <Redirect href="/(root)/(tabs)/index" />;
+// };
+
+// export default Page;
