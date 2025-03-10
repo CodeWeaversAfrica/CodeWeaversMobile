02594bd62376a335cdf4a232fba8705a591c91ff
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-10 17:42:03 +0300

    added redux toolkit: updated main index file

--- app/index.tsx
+++ app/index.tsx
@@ -19,3 +19,40 @@
 // };
 
 // export default Page;
+
+
+
+
+// import { useEffect } from 'react';
+// import { useRouter } from 'expo-router';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { RootState, AppDispatch } from '@/store';
+// import { loadAuthState } from '@/features/auth/authSlice';
+// import * as SplashScreen from 'expo-splash-screen';
+
+// SplashScreen.preventAutoHideAsync();
+
+// const Index = () => {
+//   const router = useRouter();
+//   const dispatch = useDispatch<AppDispatch>();
+//   const { isAuthenticated, loading } = useSelector((state: RootState) => state.auth);
+
+//   useEffect(() => {
+//     dispatch(loadAuthState());
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     if (!loading) {
+//       if (isAuthenticated) {
+//         router.replace('/(tabs)');
+//       } else {
+//         router.replace('/login');
+//       }
+//       SplashScreen.hideAsync();
+//     }
+//   }, [isAuthenticated, loading, router]);
+
+//   return null; // Prevent UI rendering while redirecting
+// };
+
+// export default Index;
